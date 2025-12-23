29bcac73bbd5e7534ef85234dee92232abfafa1c
jasonmichaelw <145416510+jasonmichaelw@users.noreply.github.com>
2025-12-23 22:07:49 +0800

    Update index.js

--- miniprogram-1/cloudfunctions/login/index.js
+++ miniprogram-1/cloudfunctions/login/index.js
@@ -1,22 +1,54 @@
-// cloudfunctions/login/index.js
 const cloud = require('wx-server-sdk')
-cloud.init()
+
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
 
 const db = cloud.database()
 const users = db.collection('users')
 
 exports.main = async (event, context) => {
-  const { username, password } = event
+  const { username, password } = event || {}
 
-  const res = await users.where({ username, password }).get()
-
-  if (res.data.length === 0) {
-    return { success: false, msg: "Wrong username or password" }
+  if (!username || !password) {
+    return {
+      code: 400,
+      message: 'username and password are required'
+    }
   }
 
-  return {
-    success: true,
-    msg: "Login success",
-    user: res.data[0]
+  try {
+    const res = await users.where({ username, password }).limit(1).get()
+    const user = res.data[0]
+
+    if (!user) {
+      return {
+        code: 401,
+        message: 'invalid username or password'
+      }
+    }
+
+    return {
+      code: 200,
+      message: 'login success',
+      data: {
+        userId: user._id,                 
+        username: user.username,
+        name: user.name || '',
+        sex: user.sex || '',              
+        age: user.age ?? null,
+        weight: user.weight ?? null,      
+        height: user.height ?? null,      
+        targetIntake: user.targetIntake ?? null, 
+        fitnessGoal: user.fitnessGoal || ''      // "fat_loss" | "muscle_gain" | ...
+      }
+    }
+  } catch (err) {
+    console.error('login error', err)
+    return {
+      code: 500,
+      message: 'database error',
+      error: err
+    }
   }
 }
